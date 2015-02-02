b108930f7eb38a8bcd61c904bae328f4078b3728
Orfeo Da Via <orfeo.davia@microline.eu>
2015-02-02 22:01:54 +0100

    Added readDM.
    Changed dmPeek into peekDM.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # dolina
-D implementation of Omron HostLink Protocol, a communication protocol for use with PLC's made by Omron.
+D implementation of HostLink, a communication protocol for use with PLC's made by Omron.
 
 # Libraries
-* [harbored-mod A documentation generator](https://github.com/kiith-sa/harbored-mod)
-* [unit-threaded](https://github.com/atilaneves/unit-threaded.git)
 * [serial port library](https://github.com/NCrashed/serial-port)
+* [unit-threaded](https://github.com/atilaneves/unit-threaded.git)
 * [DMocks-revived](https://github.com/QAston/DMocks-revived)
 
 # References
-* [Wiki page](https://en.wikipedia.org/wiki/HostLink_Protocol)
+* [harbored-mod A documentation generator](https://github.com/kiith-sa/harbored-mod)
+* [Wiki page about HostLink](https://en.wikipedia.org/wiki/HostLink_Protocol)
--- src/dolina/util.d
+++ src/dolina/util.d
@@ -1,19 +1,30 @@
+/**
+* Utility functions to convert DM
+*/
 module dolina.util;
 
 import std.array;
 import std.bitmanip;
 import std.system; // for Endian
 
+
+/// Bytes per each DM. A DM is a `ushort`.
+enum BYTES_PER_DM = 2;
+
 /**
 * Converts an array into ubyte array.
 * 
-* Omron stores data in LittleEndian format
+* Omron stores data in LittleEndian format.
+*
+* Examples:
+* --------------------
+*  ushort[] buf = [0x8034, 0x2010];
+*  assert(buf.toBytes!ushort() == [0x34, 0x80, 0x10, 0x20]);
+* --------------------
 *
 * Params:  words = array to convert
-*			 
 *
 * Returns: bytes rapresentation
-*
 */
 ubyte[] toBytes(T)(T[] words) {
    auto buffer = appender!(const(ubyte)[])();
@@ -33,77 +44,96 @@ ubyte[] toBytes(T)(T[] words) {
    [0x010464].toBytes!uint().shouldEqual([0x64, 0x04, 1, 0]);
 }
 
-/* 
-   FIX: 
-ubyte[] readBytes(T)(T[] words) {
-   auto buffer = appender!(const(ubyte)[])();
-   while (!words.empty) {
-      buffer.append!(T, Endian.littleEndian)(words.front);
-      words.popFront();
-   }
-   return buffer.data;
-} unittest {
-   import unit_threaded;
-   [0x8034].readBytes!ushort().shouldEqual([0x34, 0x80]);
-
-   ushort[] buf = [0x8034, 0x2010];
-   buf.readBytes!ushort().shouldEqual([0x34, 0x80, 0x10, 0x20]);
-   buf.length.shouldEqual(2);
-
-   [0x8034].readBytes!uint().shouldEqual([0x34, 0x80, 0, 0]);
-   [0x010464].readBytes!uint().shouldEqual([0x64, 0x04, 1, 0]);
-}
-*/
-
 /**
- * Takes an array dm of words ($(D ushort)) and converts the first $(D T.sizeof) bytes to
- * $(D T). The array is not consumed.
+ * Takes an array of DM (words ushort)) and converts the first `T.sizeof / 2`
+ * DM to `T`. 
+ * The array is not consumed.
  * 
  * Params:
- * T = The integral type to convert the first $(D T.sizeof) bytes to.
- * words = The array of word to convert
- * index = The index to start reading from (instead of starting at the front).
+ * T = The integral type to convert the first `T.sizeof / 2` words to.
+ * words = The array of DM to convert
+ *
+ * Examples:
+ * --------------------
+ * assert([0x645A, 0x3ffb].peekDM!float() == 1.964F);
+ * --------------------
  */
-T dmPeek(T)(ushort[] words) {
-   return dmPeek!T(words, 0);
+T peekDM(T)(ushort[] words) {
+   return peekDM!T(words, 0);
 } unittest {
    import unit_threaded;
-   [0x645A, 0x3ffb].dmPeek!float.shouldEqual(1.964F);
+   [0x645A, 0x3ffb].peekDM!float.shouldEqual(1.964F);
 }
 
-/++ ditto +/
-T dmPeek(T)(ushort[] words, size_t index) {
-   enum BYTES_PER_DM = 2;
+/**
+ * Takes an array of DM (words ushort)) and converts the first `T.sizeof / 2`
+ * DM to `T`. 
+ * The array is not consumed.
+ * 
+ * Params:
+ * T = The integral type to convert the first `T.sizeof / 2` words to.
+ * words = The array of DM to convert
+ * index = The index to start reading from (instead of starting at the front).
+ */
+T peekDM(T)(ushort[] words, size_t index) {
    ubyte[] buffer = toBytes(words);
    return buffer.peek!(T, Endian.littleEndian)(index * BYTES_PER_DM);
 } unittest {
    import unit_threaded;
-   [0x645A, 0x3ffb].dmPeek!float(0).shouldEqual(1.964F);
+   [0x645A, 0x3ffb].peekDM!float(0).shouldEqual(1.964F);
+   [0, 0, 0x645A, 0x3ffb].peekDM!float(2).shouldEqual(1.964F);
+   [0, 0, 0x645A, 0x3ffb].peekDM!float(0).shouldEqual(0);
+   [0x80, 0, 0].peekDM!ushort(0).shouldEqual(128);
+   [0xFFFF].peekDM!short(0).shouldEqual(-1);
+   [0xFFFF].peekDM!ushort(0).shouldEqual(65535);
+   [0xFFF7].peekDM!ushort(0).shouldEqual(65527);
+   [0xFFF7].peekDM!short(0).shouldEqual(-9);
+   [0xFFFB].peekDM!short(0).shouldEqual(-5);
+   [0xFFFB].peekDM!ushort(0).shouldEqual(65531);
+   [0x8000].peekDM!short(0).shouldEqual(-32768);
 }
 
-/+ fix 
 /**
- * Takes an array dm of words ($(D ushort)) and converts the first $(D T.sizeof) bytes to
- * $(D T).  The $(D T.sizeof) bytes which are read are consumed from
+ * Takes an array of DM (words ushort)) and converts the first `T.sizeof / 2`
+ * DM to `T`. 
+ * The `T.sizeof / 2` words which are read are consumed from
  * the array.
  * 
  * Params:
- * T = The integral type to convert the first $(D T.sizeof) bytes to.
- * words = The array of word to convert
+ * T = The integral type to convert the first `T.sizeof / 2` words to.
+ * words = The array of words to convert
  */
-T dmRead(T)(ushort[] words) {
-   ubyte[] buffer = toBytes(words);
-   return buffer.read!(T, Endian.littleEndian);
+T readDM(T)(ref ushort[] words) if (T.sizeof > 1) {
+   enum NO_OF_DM = T.sizeof / BYTES_PER_DM;
+
+   ushort[NO_OF_DM] dm;
+   foreach(ref e; dm) {
+      e = words.front;
+      words.popFront();
+   }
+
+   const ubyte[T.sizeof] bytes = dm.toBytes();
+   return littleEndianToNative!T(bytes);
 } unittest {
    import unit_threaded;
    ushort[] buffer = [0x645A, 0x3ffb];
    buffer.length.shouldEqual(2);
-   buffer.dmRead!float.shouldEqual(1.964F);
-   // FIX:    buffer.length.shouldEqual(0);
+   buffer.readDM!float.shouldEqual(1.964F);
+   buffer.length.shouldEqual(0);
 
-   ubyte[] bb = buffer.toBytes();
-   bb.length.shouldEqual(4);
-   bb.read!(float, Endian.littleEndian).shouldEqual(1.964F);
-   bb.length.shouldEqual(0);
+   ushort[] shortBuffer = [0x645A];
+   shortBuffer.length.shouldEqual(1);
+   shortBuffer.readDM!float().shouldThrow!Error;
+
+   ushort[] bBuffer = [0x0001, 0x0002, 0x0003];
+   bBuffer.length.shouldEqual(3);
+
+   bBuffer.readDM!ushort.shouldEqual(1);
+   bBuffer.length.shouldEqual(2);
+   
+   bBuffer.readDM!ushort.shouldEqual(2);
+   bBuffer.length.shouldEqual(1);
+
+   bBuffer.readDM!ushort.shouldEqual(3);
+   bBuffer.length.shouldEqual(0);
 }
-+/
